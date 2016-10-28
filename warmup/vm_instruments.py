1f27c6f17fe0a5deee9d611c512c5516a3207750
Sarah Mount <sarah.mount@kcl.ac.uk>
2016-10-28 19:20:22 +0100

    Parse and plot PyPy instrumentation data.

--- warmup/vm_instruments.py
+++ warmup/vm_instruments.py
@@ -74,7 +74,51 @@ class HotSpotInstrumentParser(VMInstrumentParser):
             self.chart_data.append([ChartData('GC (secs)', times_secs, 'GC events')])
 
 
+class PyPyInstrumentParser(VMInstrumentParser):
+    """Parser for PyPy instrumentation data."""
+
+    def __init__(self, instr_data):
+        VMInstrumentParser.__init__(self, 'PyPy')
+        self.instr_data = instr_data['raw_vm_events'] if instr_data else None
+        self.parse_instr_data()
+
+    def parse_instr_data(self):
+        if self.instr_data is None:
+            return None
+        for p_exec in xrange(len(self.instr_data)):
+            iterations = {'gc': list(), 'jit': list()}
+            for inproc_iter_num, node in enumerate(self.instr_data[p_exec]):
+                event_type, start_time, stop_time, children = node
+                assert start_time == stop_time == None
+                iteration = {'gc': 0, 'jit': 0}
+                for child in children:
+                    self._parse_node(child, iteration)
+                iterations['gc'].append(iteration['gc'])
+                iterations['jit'].append(iteration['jit'])
+            self.chart_data.append([ChartData('GC', iterations['gc'], 'GC events') ,
+                                    ChartData('JIT', iterations['jit'], 'JIT tracing')])
+
+    def _parse_node(self, node, info):
+        """Recurse the event tree summing time spent in gc and tracing.
+        """
+        event_type, start_time, stop_time, children = node
+        assert event_type != 'root'
+        child_time = 0
+        for child in children:
+            child_time += self._parse_node(child, info)
+        gross_time = stop_time - start_time
+        net_time = gross_time - child_time
+        if event_type.startswith('gc-'):
+            info['gc'] += net_time
+        elif event_type.startswith('jit-'):
+            info['jit'] += net_time
+        else:
+            print 'WARNING: unknown event in PyPy instrumentation: %s' % event_type
+        return net_time
+
+
 # Mapping from VM name -> parser class.
 # This enables the main scripts to parse instrumentation data based only
 # on the vm:bench:language triplets found in Krun data files.
-INSTRUMENTATION_PARSERS = {'Hotspot': HotSpotInstrumentParser}
+INSTRUMENTATION_PARSERS = { 'Hotspot': HotSpotInstrumentParser,
+                            'PyPy': PyPyInstrumentParser, }
