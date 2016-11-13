bcad328484b32ffa229627451093d9268f58468f
Edd Barrett <vext01@gmail.com>
2016-11-13 16:39:58 +0000

    Fix bug in bootstrap.

--- warmup/statistics.py
+++ warmup/statistics.py
@@ -3,14 +3,15 @@ import math
 from kalibera import Data
 
 CONFIDENCE = 0.99
+ITERATIONS = 10000
 
 
 def mean(seq):
     return math.fsum(seq) / len(seq)
 
 
-def bootstrap_confidence_interval(seq, confidence=CONFIDENCE):
+def bootstrap_confidence_interval(seq, confidence=CONFIDENCE, iterations=ITERATIONS):
     size = len(seq)
     data = Data({(): seq}, [size])
-    result = data.bootstrap_confidence_interval(size, confidence=str(confidence))
+    result = data.bootstrap_confidence_interval(iterations, confidence=str(confidence))
     return result.median, result.error
