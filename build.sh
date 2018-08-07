24f90e6fe51b112b715699f28b1793df3cf8ae3c
Sarah Mount <sarah.mount@kcl.ac.uk>
2018-08-07 13:51:14 +0100

    Improve build.sh. Simplify directory where R packages are installed. pip works on Debian 8, 9 and Ubuntu. This change ensures that rpy2 is always installed locally, in the work/ directory inside this repo. Use portable comparison operators throughout. Use lsb_release to determine Debian version. Fix some issues found by shellcheck.

--- build.sh
+++ build.sh
@@ -42,30 +42,30 @@
 set -e
 
 # Local directories for installing Python and R packages.
-R_INST_DIR=`pwd`/work/R-inst
+R_LIB_DIR=`pwd`/work/rlibs
 PIP_TARGET_DIR=`pwd`/work/pylibs
 
 # Check requirements.
 which git > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [ $? != 0 ]; then
     echo "git must be installed" >&2
     exit 1
 fi
 
 which python2.7 > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [ $? != 0 ]; then
     echo "python2.7 must be installed" >&2
     exit 1
 fi
 
 python2.7 -c "import setuptools" > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [ $? != 0 ]; then
     echo "setuptools for python2.7 must be installed" >&2
     exit 1
 fi
 
 which R > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [ $? != 0 ]; then
     echo "R version 3.3.1 or later must be installed"
     exit 1
 fi
@@ -74,29 +74,29 @@ version() {
     echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
 
-if [ $(version `R --version | awk 'NR==1 {print $3}'`) -le $(version "3.3.1") ]; then
+if [ $(($(version "$(R --version | awk 'NR==1 {print $3}')") <= $(version "3.3.1"))) = 1 ]; then
     echo "R version 3.3.1 or later must be installed"
     exit 1
 fi
 
 # Install R changepoint package.
-mkdir -p ${R_INST_DIR}/lib/R/library
-echo "install.packages('devtools', lib='${R_INST_DIR}/lib/R/library', repos='http://cran.us.r-project.org')" | R_LIBS_USER=${R_INST_DIR}/lib/R/library/ R --no-save
-echo "install.packages('MultinomialCI', lib='${R_INST_DIR}/lib/R/library', repos='http://cran.us.r-project.org')" | R_LIBS_USER=${R_INST_DIR}/lib/R/library/ R --no-save
-echo "devtools::install_git('git://github.com/rkillick/changepoint', branch = 'master')" | R_LIBS_USER=${R_INST_DIR}/lib/R/library/ R --no-save
+mkdir -p ${R_LIB_DIR}
+echo "install.packages('devtools', lib='${R_LIB_DIR}', repos='http://cran.us.r-project.org')" | R_LIBS_USER=${R_LIB_DIR} R --no-save
+echo "install.packages('MultinomialCI', lib='${R_LIB_DIR}', repos='http://cran.us.r-project.org')" | R_LIBS_USER=${R_LIB_DIR} R --no-save
+echo "devtools::install_git('git://github.com/rkillick/changepoint', branch = 'master')" | R_LIBS_USER=${R_LIB_DIR} R --no-save
 
 # Install rpy2 Python package.
-DEB8_HACK=0
-if [ -f /etc/debian_version ]; then
-    DEB_MAJOR_V=`cat /etc/debian_version | cut -d. -f1`
-    if [ "${DEB_MAJOR_V}" = "8" ]; then
-        DEB8_HACK=1
+DEB8_HACK="0"
+if [ "$(lsb_release -si)" = "Debian" ]; then
+    DEB_MAJOR_V=`lsb_release -sr | cut -c1`
+    if [ "${DEB_MAJOR_V}" = "8" ] || [ "${DEB_MAJOR_V}" = "9" ]; then
+        DEB8_HACK="1"
     fi
 fi
 
 if [ "${DEB8_HACK}" = "1" ]; then
-    # Debian 8
-    pip install -t ${PIP_TARGET_DIR} "rpy2==2.8.5"
+    # Debian 8 or 9
+    pip install --system --target "${PIP_TARGET_DIR}" "rpy2==2.8.5"
 else
-    pip install --install-option="--prefix=${PIP_TARGET_DIR}" "rpy2==2.8.5"
+    pip install --target "${PIP_TARGET_DIR}" "rpy2==2.8.5"
 fi
